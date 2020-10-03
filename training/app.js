ccdfe30ac2b739da59e38a44d93aae256b6dc638
Danny Cheun <dcheun@gmail.com>
2020-10-03 12:46:45 -0700

    feat: Add Sequelize functionality for Cart and Order
    Define models, relationships, and updated controller functions.
    Convert cart.js to use sequelize.
    Additional CSS.
    Updated views.

--- training/app.js
+++ training/app.js
@@ -7,6 +7,10 @@ const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
 
 const app = express();
 
@@ -39,6 +43,13 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
   // .sync({ force: true })
@@ -55,6 +66,9 @@ sequelize
     return user;
   })
   .then((user) => {
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(PORT, () => {
       console.log(`Server started on port ${PORT}`);
     });
